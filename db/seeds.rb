7aa47ccc1001a12e786555c3e87dae779697313c
ethanryan <ethanryan@gmail.com>
2018-01-03 18:46:49 -0500

    updated seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,18 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-
 url_1 = Url.create([
   url: "https://www.rottentomatoes.com/m/the_dark_knight"
 ])
 
 url_2 = Url.create([
   url: "https://www.rottentomatoes.com/m/it_comes_at_night"
+])
+
+url_3 = Url.create([
+  url: "https://www.rottentomatoes.com/m/hulk/"
+])
+
+url_4 = Url.create([
+  url: "https://www.rottentomatoes.com/tv/black_mirror"
 ])
