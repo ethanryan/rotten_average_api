c4202f98ccc609d8f45fa36b27846d9e963fc689
ethanryan <ethanryan@gmail.com>
2018-01-03 15:10:06 -0500

    added nokogiri gem and parseHTML function

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -5,4 +5,21 @@ class UrlsController < ApplicationController
     render json: urls
   end
 
+  #url = "https://www.rottentomatoes.com/m/hulk/"
+  #url = "https://www.rottentomatoes.com/m/it_comes_at_night/"
+
+  def parseHTML(url)
+    require 'open-uri'
+    doc = Nokogiri::HTML( open(url) )
+
+    title = doc.title
+    puts "doc title is: " + title
+
+    tomato_meter = doc.css("span.superPageFontColor")[0]
+    puts "tomato_meter is: " + tomato_meter
+
+    audience_score = doc.css("span.superPageFontColor")[12]
+    puts "audience_score is: " + audience_score
+  end
+
 end
