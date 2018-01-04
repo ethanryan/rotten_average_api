3e81d151208d9fe367f04858a19ab0b72cb0f438
ethanryan <ethanryan@gmail.com>
2018-01-04 00:27:29 -0500

    changed table column from url to link

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180103212030) do
+ActiveRecord::Schema.define(version: 20180104052543) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "urls", force: :cascade do |t|
-    t.string "url"
+    t.string "link"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "title"
